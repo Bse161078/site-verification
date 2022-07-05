f49c412f525003a5781b20e1480dc80a0a4f6cd4
abdullahzahid321456 <abdullahzahid321456@gmail.com>
2022-07-05 16:37:26 +0500

    initial commit

---
+++ app.js
@@ -0,0 +1,138 @@
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
+const {google} = require('googleapis');
+const OAuth2Client =google.auth.OAuth2;
+
+
+var app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+//app.use(express.static(path.join(__dirname, 'public')));
+
+
+
+const oauth2Client=() =>{
+    return new google.auth.OAuth2(
+        "66805527862-vtcb1djnqbt1e219lfjo4bepducecsqn.apps.googleusercontent.com",
+        "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",
+        'https://site-verification-b1661.firebaseapp.com/__/auth/handler'
+    );
+}
+
+const oauth2GetAuthorizationCode = (req,res)=> {
+
+    const client = oauth2Client();
+
+    const url = client.generateAuthUrl({
+        access_type: 'offline',
+        scope: [
+            'https://www.googleapis.com/auth/siteverification'
+        ]
+    });
+    res.status(200).send({url: url});
+};
+
+const oauth2GetAccessToken= (req, res) => {
+
+    const client = oauth2Client();
+    const code = req.query.code;
+
+    client.getToken(code, (err, tokens) => {
+        if (!err) {
+            res.status(200).send({tokens});
+        } else {
+            console.error('Error while getting access token:', err);
+            res.sendStatus(500);
+        }
+    });
+};
+
+
+const oauth2ClientWithCredentials=(access_token,refresh_token)=> {
+    const client = oauth2Client();
+
+    client.setCredentials({
+        access_token,
+        refresh_token
+    });
+
+    return client;
+}
+
+
+
+
+const invokeSiteVerificationApi=async (req,res)=> {
+
+    const client = oauth2ClientWithCredentials(req.body.access_token,req.body.refresh_token);
+
+    const siteVerification = google.siteVerification({
+        version: 'v1',
+        auth: client
+    });
+
+    try{
+        await siteVerification.webResource.insert({
+            verificationMethod: 'DNS_TXT',
+            requestBody: {
+                site: {
+                    type: 'INET_DOMAIN',
+                    identifier: req.body.domain,
+                },
+            }
+        });
+    }catch (e) {
+        res.json(e);
+    }
+}
+
+
+const generateTokenToVerifyDomain=async(req,res)=>{
+    const client = oauth2ClientWithCredentials(req.body.access_token,req.body.refresh_token);
+
+    const siteVerification = google.siteVerification({
+        version: 'v1',
+        auth: client
+    });
+
+    try{
+        const { data } = await siteVerification.webResource.getToken({
+            requestBody: {
+                site: {
+                    type: 'INET_DOMAIN',
+                    identifier: req.body.domain,
+                },
+                verificationMethod: 'DNS_TXT',
+            }
+        });
+        const token = data.token;
+        res.json({data});
+    }catch (e) {
+        res.json(e);
+    }
+
+}
+
+app.get('/', oauth2GetAuthorizationCode);
+app.get('/token', oauth2GetAccessToken);
+app.post('/verify', invokeSiteVerificationApi);
+app.post('/generate-token', generateTokenToVerifyDomain);
+
+app.use('/users', usersRouter);
+
+
+app.listen(3000)
+
+module.exports = app;
+
+
+
